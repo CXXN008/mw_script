035407c7c49c5fe325fe685dae2047ebf0a2f49c
cccccccccccccccccccccccccccccccccccccc <linus3389@gmail.com>
2020-12-28 17:08:28 +0800

    slider fix

---
+++ testzone_singlepage/index.js
@@ -0,0 +1,33 @@
+const com_prefix = '//cdn.jsdelivr.net/gh/cccccccccccccccccccccccccccccccccccccc/mw_script@latest/release/'
+
+// what components do we need
+const coms = document.querySelectorAll('[id^=mwcom]')
+
+for (const c of coms) {
+	//  load js
+	let script = document.createElement('script')
+
+	script.type = 'text/javascript'
+	script.src =
+		// `${com_prefix}${c.id.replace(
+		// 	/mwcom-|-/g,
+		// 	(m) => name_fix[m]
+		// )}.js`
+		`${com_prefix}${c.id.replaceAll('-', '_')}.js`
+
+	document.head.appendChild(script)
+
+	if (eval(c.getAttribute('mwstyle'))) {
+		//  load css if need
+		let link = document.createElement('link')
+		link.rel = 'stylesheet'
+		link.href = `${com_prefix}${c.id.replaceAll('-', '_')}.css`
+		document.head.appendChild(link)
+	}
+}
+
+// let script = document.createElement('script')
+// script.type = 'text/javascript'
+// script.src = 'https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js'
+
+// document.head.appendChild(script)
