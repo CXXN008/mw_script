5e9987e414b4b8d93e0c5bf2b93c2ee5c19937c8
cccccccccccccccccccccccccccccccccccccc <linus3389@gmail.com>
2020-12-30 17:52:34 +0800

    black quote

---
+++ com_black_quote/index.js
@@ -0,0 +1,23 @@
+let com = document.querySelector('#mwcom-black-quote')
+let params = {}
+if (com.innerText.search('mwcom-config:') > 0) {
+	console.log(com.innerText.replace(/.+mwcom-config:/g, ''))
+	params = eval(com.innerText.replace(/.+mwcom-config:/g, ''))
+}
+let style = document.createElement('style')
+style.innerHTML = `
+.black-quote {
+    background-color: black;
+    color: black;
+    transition: color .3s;
+
+}
+.black-quote:hover{
+    color: white;
+    transition: color .3s;
+}
+`
+document.head.append(style)
+document.body.innerHTML = document.body.innerHTML.replace(/<black-quote>/g,'<span class=\'black-quote\'>')
+document.body.innerHTML = document.body.innerHTML.replace(/<\/black-quote>/g,'</span>')
+
