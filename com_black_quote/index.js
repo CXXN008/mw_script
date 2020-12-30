6cda3b2198ce899f87eed4c17e32aa17a83730bd
cccccccccccccccccccccccccccccccccccccc <linus3389@gmail.com>
2020-12-30 18:23:13 +0800

    black quote params apply

--- com_black_quote/index.js
+++ com_black_quote/index.js
@@ -1,23 +1,26 @@
 let com = document.querySelector('#mwcom-black-quote')
 let params = {}
 if (com.innerText.search('mwcom-config:') > 0) {
-	console.log(com.innerText.replace(/.+mwcom-config:/g, ''))
-	params = eval(com.innerText.replace(/.+mwcom-config:/g, ''))
+    params = eval(com.innerText.replace(/.+mwcom-config:/g, ''))
+    console.log(params);
 }
 let style = document.createElement('style')
 style.innerHTML = `
 .black-quote {
-    background-color: black;
-    color: black;
-    transition: color .3s;
+    background-color: ${params['background_color']};
+    color: ${params['background_color']};
+    transition: color ${params['animation_duration']}s;
 
 }
-.black-quote:hover{
-    color: white;
-    transition: color .3s;
+.black-quote:hover {
+    color: ${params['text_color']};
+    transition: color ${params['animation_duration']}s;
 }
 `
+console.log(style);
+// apply style
 document.head.append(style)
+// replace text
 document.body.innerHTML = document.body.innerHTML.replace(/&lt;black-quote&gt;/g,'<span class=\'black-quote\'>')
 document.body.innerHTML = document.body.innerHTML.replace(/&lt;\/black-quote&gt;/g,'</span>')
 
