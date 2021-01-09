d6b28422660c2a518be563faf5046f7f3099d3e3
cccccccccccccccccccccccccccccccccccccc <linus3389@gmail.com>
2021-01-10 05:24:13 +0800

    fix black-quote lead to mediawiki self css does not work.

--- com_black_quote/index.js
+++ com_black_quote/index.js
@@ -5,22 +5,29 @@ if (com.innerText.search('mwcom-config:') > 0) {
     console.log(params);
 }
 let style = document.createElement('style')
+// <a>tag 特殊处理，其他一律按配置颜色来
 style.innerHTML = `
-.black-quote {
+.black-quote,.black-quote>* {
     background-color: ${params['background_color']};
     color: ${params['background_color']};
     transition: color ${params['animation_duration']}s;
-
 }
-.black-quote:hover {
+
+.black-quote:hover,.black-quote:hover>* {
     color: ${params['text_color']};
     transition: color ${params['animation_duration']}s;
+}
+
+.black-quote:hover>a{
+    color: #03A9F4;
 }
 `
 console.log(style);
 // apply style
 document.head.append(style)
+
+// dynamic replace html lead to mediawiki self css does not work
 // replace text
-document.body.innerHTML = document.body.innerHTML.replace(/&lt;black-quote&gt;/g,'<span class=\'black-quote\'>')
-document.body.innerHTML = document.body.innerHTML.replace(/&lt;\/black-quote&gt;/g,'</span>')
+// document.body.innerHTML = document.body.innerHTML.replace(/&lt;black-quote&gt;/g,'<span class=\'black-quote\'>')
+// document.body.innerHTML = document.body.innerHTML.replace(/&lt;\/black-quote&gt;/g,'</span>')
 
